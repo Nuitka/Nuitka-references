4b00f9368de5e3a0bcf75ec73f495983016362a7
Kay Hayen <kay.hayen@gmail.com>
2024-10-09 13:50:19 +0200

    Removed more noise files

---
+++ cleanup.sh
@@ -0,0 +1,12 @@
+find . -name Doc -exec rm -rf {} \;
+find . -name .devcontainer -exec rm -rf {} \;
+find . -name .mailmap -delete
+find . -name .pre-commit-config.yaml -delete
+find . -name .readthedocs.yml -delete
+find . -name aclocal.m4 -delete
+find . -name config.guess -delete
+find . -name config.sub -delete
+find . -name configure -delete
+find . -name configure.ac -delete
+find . -name install-sh -delete
+find . -name Makefile.pre.in -delete
