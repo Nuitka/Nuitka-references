8751b105f73205a1f70dae95dac1c4f66c23d091
Kay Hayen <kay.hayen@gmail.com>
2024-10-15 09:01:37 +0200

    Remove more useless files

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,7 @@
 find . -name Doc -exec rm -rf {} \;
+find . -name InternalDocs -exec rm -rf {} \;
 find . -name .devcontainer -exec rm -rf {} \;
+find . -name Tools -exec rm -rf {} \;
 find . -name .mailmap -delete
 find . -name .pre-commit-config.yaml -delete
 find . -name .readthedocs.yml -delete
@@ -8,5 +10,6 @@ find . -name config.guess -delete
 find . -name config.sub -delete
 find . -name configure -delete
 find . -name configure.ac -delete
+find . -name pyconfig.h.in -delete
 find . -name install-sh -delete
 find . -name Makefile.pre.in -delete
